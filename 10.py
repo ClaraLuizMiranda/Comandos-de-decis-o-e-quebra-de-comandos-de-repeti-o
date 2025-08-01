ab78fbf70fad5e0669ae825ae01a13027ccadf3a
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 21:24:45 -0300

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,18 @@
+Escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia da semana correspondente a este numero. Isto é, domingo se 1, segunda-feira se 2, e assim por diante.
+
+n=int(input())
+if n==2:
+    print('Segunda-feira')
+if n==3:
+    print('Terca-feira')
+if n==4:
+    print('Quarta-feira')
+if n==5:
+    print('Quinta-feira')
+if n==6:
+    print('Sexta-feira')
+if n==7:
+    print('Sabado')
+if n==1:
+    print('Domingo')
+  
