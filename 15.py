725180f068d092df6c2020df99a277f50005c693
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 21:28:19 -0300

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,12 @@
+Escreva um programa que leia um número inteiro e calcule a soma de todos os divisores desse número, com exceção dele próprio. Exemplo: a soma dos divisores de 66 é 1 + 2 + 3 + 6 + 11 + 22 + 33 = 78.
+
+def funcao(n):
+    soma = 0
+    for i in range(1, n):
+        if n % i == 0:
+            soma += i
+    return soma
+
+n2 = int(input())
+soma2 = funcao(n2)
+print(soma2)
