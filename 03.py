c855b5d0824d4ac777afc2ca123ef8e23a737360
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 21:17:48 -0300

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,20 @@
+Ler 4 números inteiros e calcular a soma dos que forem par.
+
+soma=0
+n1=int(input())
+n2=int(input())
+n3=int(input())
+n4=int(input())
+
+if n1 % 2 == 0:
+    soma += n1
+if n2 % 2 == 0:
+    soma += n2
+if n3 % 2 == 0:
+    soma += n3
+if n4 % 2 == 0:
+    soma += n4
+print(soma)
+    
+        
+    
