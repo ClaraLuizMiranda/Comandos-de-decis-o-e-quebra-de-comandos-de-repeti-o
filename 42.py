ac843ad134ad1a657679f3e19f0cadf66d16140b
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 21:44:30 -0300

    Create 42.py

---
+++ 42.py
@@ -0,0 +1,8 @@
+Ler uma frase e contar quantos caracteres são brancos. Não use nenhuma funcionalidade do python que já faça isso.
+
+t=input()
+
+espacos=t.count(' ')
+
+if espacos>=0:
+    print(espacos)
