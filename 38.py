6335fa5ec42d6178936d61f3e2af892ffbc9218f
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 21:42:27 -0300

    Create 38.py

---
+++ 38.py
@@ -0,0 +1,8 @@
+Crie uma função que diga se duas strings são iguais ou não, analisando caractere a caractere.
+
+def funcao(x1, x2):
+    if x1==x2:
+        return True
+    else:
+        return False
+          
