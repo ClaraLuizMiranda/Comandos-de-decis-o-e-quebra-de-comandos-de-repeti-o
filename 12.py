ae1576f1f4abda623dbfd126b74f3126085c094f
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 21:26:38 -0300

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,19 @@
+Faça um programa que leia um valor digitado pelo usuário e imprima os resultados com base na tabela.
+◦ 1: Bom Dia - Boa Tarde - Boa Noite
+◦ 2: Por Favor :) - Com Licença :D - Muito Obrigado ;)
+◦ 3: Por Gentileza - Você poderia - Desculpe
+◦ 4: Boa Sorte - Tenha Fé
+◦ Outro: Estudar vale muito a pena não é !?
+
+n = int(input())
+
+if n == 1:
+    print("Bom Dia - Boa Tarde - Boa Noite")
+elif n == 2:
+    print("Por Favor :) - Com Licenca :D - Muito Obrigado ;)")
+elif n == 3:
+    print("Por Gentileza - Voce poderia - Desculpe")
+elif n == 4:
+    print("Boa Sorte - Tenha Fe")
+else:
+    print("Estudar vale muito a pena!")
